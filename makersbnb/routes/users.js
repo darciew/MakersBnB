a0234711b518f28b1549b1a403b99feb5bc293fb
tom miller <thomas.j.c.miller@outlook.com>
2018-10-09 16:29:26 +0100

    displays users

--- makersbnb/routes/users.js
+++ makersbnb/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var users = ['Billy', 'Bobby', 'Barry']
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+  res.json(users);
 });
+
+router.get('/:id', function(req, res) {
+  res.json(users[req.params.id])
+})
 
 module.exports = router;
